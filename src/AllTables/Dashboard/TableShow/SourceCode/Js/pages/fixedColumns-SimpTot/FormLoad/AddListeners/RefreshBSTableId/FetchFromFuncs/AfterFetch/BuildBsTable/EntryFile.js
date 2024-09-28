16b618595cacfb9262da86fb4c9ee41ce2998754
keshavsoft <nknnkumar@yahoo.com>
2024-09-28 18:46:42 +0530

    totals working

--- src/AllTables/Dashboard/TableShow/SourceCode/Js/pages/fixedColumns-SimpTot/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/AfterFetch/BuildBsTable/EntryFile.js
+++ src/AllTables/Dashboard/TableShow/SourceCode/Js/pages/fixedColumns-SimpTot/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/AfterFetch/BuildBsTable/EntryFile.js
@@ -1,4 +1,5 @@
 import optionsJson from './options.json' with {type: 'json'};
+import { StartFunc as Rows } from "./ColumnHandlers/Rows/EntryFile.js";
 
 const StartFunc = ({ inData }) => {
     const LocalDataAsArray = inData;
@@ -6,6 +7,7 @@ const StartFunc = ({ inData }) => {
     var $table = $('#table');
 
     optionsJson.data = LocalDataAsArray;
+    optionsJson.columns[1].footerFormatter = Rows;
 
     $table.bootstrapTable(optionsJson);
 };
