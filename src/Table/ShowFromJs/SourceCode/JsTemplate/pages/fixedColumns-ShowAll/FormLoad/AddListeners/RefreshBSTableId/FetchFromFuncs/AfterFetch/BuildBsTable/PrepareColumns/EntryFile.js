0133498e417e88960aab57d82b516b301ed4ac47
keshavsoft <nknnkumar@yahoo.com>
2024-10-24 21:08:55 +0530

    showfromjs working good with simple

--- src/Table/ShowFromJs/SourceCode/JsTemplate/pages/fixedColumns-ShowAll/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/AfterFetch/BuildBsTable/PrepareColumns/EntryFile.js
+++ src/Table/ShowFromJs/SourceCode/JsTemplate/pages/fixedColumns-ShowAll/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/AfterFetch/BuildBsTable/PrepareColumns/EntryFile.js
@@ -1,18 +1,12 @@
-import { StartFunc as SerialColumn } from "./SerialColumn/EntryFile.js";
-import { StartFunc as CallColumn } from "./CallColumn/EntryFile.js";
-import { StartFunc as WhatsAppColumn } from "./WhatsAppColumn/EntryFile.js";
-import { StartFunc as GDrive } from "./GDrive/EntryFile.js";
+const StartFunc = ({ inFirstRow }) => {
+    let jVarLocalReturnArray = [];
 
-const StartFunc = ({ inColumnsArray }) => {
-    let jVarLocalReturnArray = inColumnsArray;
-
-    jVarLocalReturnArray[2].sortable = true;
-    jVarLocalReturnArray[9].sortable = true;
-
-    jVarLocalReturnArray.unshift(GDrive());
-    jVarLocalReturnArray.unshift(WhatsAppColumn());
-    jVarLocalReturnArray.unshift(CallColumn());
-    jVarLocalReturnArray.unshift(SerialColumn());
+    for (const [key, value] of Object.entries(inFirstRow)) {
+        jVarLocalReturnArray.push({
+            field: key,
+            title: key
+        });
+    }
 
     return jVarLocalReturnArray;
 };
