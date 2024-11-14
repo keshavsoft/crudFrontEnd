1d980ab947625607c03b04a2a4d1097b6c1077c6
Nalipi Sridhar Reddy <nalipisridharreddy@gmail.com>
2024-11-14 16:19:27 +0530

    CreateWithNotification v1

--- src/UnProtected/Create/Static/SourceCode/JsTemplate/pages/CreateWithNotification/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/UnProtected/Create/Static/SourceCode/JsTemplate/pages/CreateWithNotification/FormLoad/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -1,29 +1,32 @@
-import UrlJson from './url.json' with {type: 'json'};
+const messageElement = "Student Added";
+let serviceWorkerRegistration;
+
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('../JsTemplate/pages/CreateWithNotification/NotificationFuncs/service-worker.js').then(function (registration) {
+        serviceWorkerRegistration = registration;
+    });
+}
 
 let StartFunc = ({ inRowPk }) => {
-    let LocalRowPk = inRowPk;
+    webNotification.showNotification(inRowPk, {
+        serviceWorkerRegistration: serviceWorkerRegistration,
+        body: messageElement,
+        onClick: function onNotificationClicked() {
+            console.log('Notification clicked.');
+        },
+        autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
+    }, function onShow(error, hide) {
+        if (error) {
+            window.alert('Unable to show notification: ' + error.message);
+        } else {
+            console.log('Notification Shown.');
 
-    if (LocalFuncForSingleTable({ inRowPk: LocalRowPk }) === false) {
-        LocalFuncForAllTables({ inRowPk: LocalRowPk });
-    };
+            setTimeout(function hideNotification() {
+                console.log('Hiding notification....');
+                hide(); //manually close the notification (you can skip this if you use the autoClose option)
+            }, 5000);
+        }
+    });
 };
-
-const LocalFuncForSingleTable = ({ inRowPk }) => {
-    let LocalRowPk = inRowPk;
-
-    if (window.location.pathname.endsWith(`/${UrlJson.PresentUrl}`)) {
-        window.location.href = `${UrlJson.RedirectToUrl}?inRowPk=${LocalRowPk}`;
-        return true;
-    };
-
-    return false;
-};
-
-const LocalFuncForAllTables = ({ inRowPk }) => {
-    let LocalRowPk = inRowPk;
-
-    window.location.href = `${jVarGlobalTableName}${UrlJson.RedirectToUrl}?inRowPk=${LocalRowPk}`;
-};
-
 
 export { StartFunc }
